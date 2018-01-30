834e71d482b26fbeeeb485b4cfd7cd32699f1ca4
brantchyoga <brantrporter@gmail.com>
2018-01-30 00:25:32 -0800

    Added styling and connected lists with sortable

--- js/script.js
+++ js/script.js
@@ -1,46 +1,39 @@
-var list = $('<ul>');
-
-list.addClass("list");
-$('#container').append(list);
-
-// $(function  () {
-//   list.sortable();
-// });
-
+//Connects 2 separate lists using there class
+$("#tasklist, #finished").sortable({
+      connectWith: ".sortable"
+    }).disableSelection();
+//Grabs the clicked <a> tag and removes the parent
+//element <li>
 var clearItem = function() {
 	$(this).parent().remove();
-	console.log('test');
 }
-
+//Function creates li & a tags, adds a class,
+//grabs the value from the input field and
+//puts it into html list
 var addListItem = function() {
 	var removeItem = $('<a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Delete</a>');
-	var listItem = $('<li>');
-	var newListItem = listItem.text($("#tasktodo").val());
+	var newListItem = $('<li>').text($("#tasktodo").val());;
 	newListItem.addClass('toremove');
-	$(list).append(newListItem);
+//appending <a> tag to li is important for the
+//onclick function clearItem
+	newListItem.append(removeItem);
+	$('#tasklist').hide()
+			.css('opacity',0.0)
+			.append(newListItem)
+			.show('slow')
+			.animate({opacity: 1.0});
+//resets input field to nothing and focuses
 	$("#tasktodo").val('');
 	$('#tasktodo').focus();
-	newListItem.append(removeItem);
-	
 }
 
 var clearList = function() {
 	$('.toremove').remove();
 }
 
-
-
-
-
-
 $("document").ready(function() {
-	
 	$('#submit').on('click', addListItem);
 	$('#delete').on('click', clearList);
-	$('#container').on("click", 'a.',clearItem);
-	
-	
-
-
-
+//grabs the parent element of the event handler <a>
+	$('#finished').on("click", 'a',clearItem);
 });
